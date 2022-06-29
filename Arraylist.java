a8a34d2587af7c049eeef1eb5c5b60c6aee569e3
jakka sharada <sharadas672@gmail.com>
2022-06-29 22:10:04 +0530

    2nd commit

---
+++ Arraylist.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Arraylist
+{
+	public static void main(String args[])
+	{
+		ArrayList<Integer> a=new ArrayList<Integer>();//creating arraylist
+		for(int i=0;i<=10;i++)// adding elements to an array
+			a.add(i);
+		//for(int i=0;i<10;i++)
+		//	System.out.println(i);//printing array elements
+		a.remove(5);
+		for(int i=0;i<=10;i++)
+			System.out.println(i);
+	}
+}
